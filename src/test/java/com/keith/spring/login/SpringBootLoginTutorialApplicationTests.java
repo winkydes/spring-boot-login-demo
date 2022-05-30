618b120856d530fa9d55cbc7bcdf4fa73faadf17
Lam Wai To Keith <75211259+winkydes@users.noreply.github.com>
2022-05-30 12:59:51 +0800

    first commit

---
+++ src/test/java/com/keith/spring/login/SpringBootLoginTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keith.spring.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLoginTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
